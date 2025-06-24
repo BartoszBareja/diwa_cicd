b4448a359ef596022494b02a05e91486757caa4d
BartoszBareja <barejabartosz@gmail.com>
2025-06-24 15:26:47 +0200

    updated home.php

--- app/content/home.php
+++ app/content/home.php
@@ -1,3 +1,14 @@
+<?php
+header('X-Frame-Options: SAMEORIGIN');
+session_start([
+    'cookie_httponly' => true,
+    'cookie_secure' => isset($_SERVER['HTTPS']),
+]);
+header('X-Content-Type-Options: nosniff');
+header('Referrer-Policy: no-referrer-when-downgrade');
+header('Content-Security-Policy: default-src \'self\'');
+?>
+
 <div class="jumbotron">
     <div class="container"><h1>Hello, Welcome to DIWA!</h1>
         <p><strong>D</strong>eliberately <strong>I</strong>nsecure <strong>W</strong>eb <strong>A</strong>pplication</p>
@@ -13,16 +24,16 @@
             <h2>How insecure is DIWA?</h2>
             <p>DIWA is horribly insecure! The following vulnerabilities and attack vectors exist in this application:</p>
             <ul>
-                <li><a href="https://www.owasp.org/index.php/SQL_Injection" target="_blank">SQL Injection</a></li>
-                <li><a href="https://www.owasp.org/index.php/Cross-site_Scripting_(XSS)" target="_blank">XSS (Corss-Site-Scripting)</a></li>
-                <li><a href="https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)" target="_blank">CSRF (Cross-Site-Request-Forgery)</a></li>
-                <li><a href="https://www.owasp.org/index.php/Session_hijacking_attack" target="_blank">Session Hijacking</a></li>
-                <li><a href="https://www.owasp.org/index.php/Session_fixation" target="_blank">Session Fixation</a></li>
-                <li><a href="https://www.owasp.org/index.php/Content_Spoofing" target="_blank">Content Spoofing</a></li>
-                <li><a href="https://www.owasp.org/index.php/Top_10_2013-A7-Missing_Function_Level_Access_Control" target="_blank">Missing Function Level Access Control</a></li>
-                <li><a href="https://www.owasp.org/index.php/Path_Traversal" target="_blank">Path Traversal</a></li>
-                <li><a href="https://www.owasp.org/index.php/Top_10_2013-A6-Sensitive_Data_Exposure" target="_blank">Sensitive Data Exposure</a></li>
-                <li><a href="https://www.owasp.org/index.php/Brute_force_attack" target="_blank">Brute Force Attacks</a></li>
+                <li><a href="https://www.owasp.org/index.php/SQL_Injection" target="_blank" rel="noopener noreferrer">SQL Injection</a></li>
+                <li><a href="https://www.owasp.org/index.php/Cross-site_Scripting_(XSS)" target="_blank" rel="noopener noreferrer">XSS (Cross-Site-Scripting)</a></li>
+                <li><a href="https://www.owasp.org/index.php/Cross-Site_Request_Forgery_(CSRF)" target="_blank" rel="noopener noreferrer">CSRF (Cross-Site-Request-Forgery)</a></li>
+                <li><a href="https://www.owasp.org/index.php/Session_hijacking_attack" target="_blank" rel="noopener noreferrer">Session Hijacking</a></li>
+                <li><a href="https://www.owasp.org/index.php/Session_fixation" target="_blank" rel="noopener noreferrer">Session Fixation</a></li>
+                <li><a href="https://www.owasp.org/index.php/Content_Spoofing" target="_blank" rel="noopener noreferrer">Content Spoofing</a></li>
+                <li><a href="https://www.owasp.org/index.php/Top_10_2013-A7-Missing_Function_Level_Access_Control" target="_blank" rel="noopener noreferrer">Missing Function Level Access Control</a></li>
+                <li><a href="https://www.owasp.org/index.php/Path_Traversal" target="_blank" rel="noopener noreferrer">Path Traversal</a></li>
+                <li><a href="https://www.owasp.org/index.php/Top_10_2013-A6-Sensitive_Data_Exposure" target="_blank" rel="noopener noreferrer">Sensitive Data Exposure</a></li>
+                <li><a href="https://www.owasp.org/index.php/Brute_force_attack" target="_blank" rel="noopener noreferrer">Brute Force Attacks</a></li>
             </ul>
             <h2>What can I do now?</h2>
             <p>Try to find out more about the possible attack Vectors shown above. Then just feel free to hack everything on this website you can find, e.g.:</p>
