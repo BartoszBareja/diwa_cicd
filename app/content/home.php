8551c7bb845005e80c0244175db443aa650004f7
BartoszBareja <barejabartosz@gmail.com>
2025-06-24 15:29:30 +0200

    updated home.php

--- app/content/home.php
+++ app/content/home.php
@@ -1,12 +1,15 @@
 <?php
+header_remove('X-Powered-By');
+
 header('X-Frame-Options: SAMEORIGIN');
+header('X-Content-Type-Options: nosniff');
+header('Referrer-Policy: no-referrer-when-downgrade');
+header("Content-Security-Policy: default-src 'self'");
+
 session_start([
     'cookie_httponly' => true,
     'cookie_secure' => isset($_SERVER['HTTPS']),
 ]);
-header('X-Content-Type-Options: nosniff');
-header('Referrer-Policy: no-referrer-when-downgrade');
-header('Content-Security-Policy: default-src \'self\'');
 ?>
 
 <div class="jumbotron">
